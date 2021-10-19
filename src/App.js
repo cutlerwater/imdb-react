397e84dca8c4a4a8a0d17816e3d469c150fe4832
cutlerwater <cutlerwater2@live.com>
2021-10-19 18:29:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Components
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+import Login from './components/Login';
+// Context
+import UserProvider from './context';
+// Styles
+import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+    </UserProvider>
+  </Router>
+);
 
 export default App;
